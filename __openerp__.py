2b1c0f72916da5d0b3649ae9c040115f5fa06873
fabiancho32 <the-fabiancho@hotmail.com>
2017-03-09 10:00:15 -0500

    iniciando el repositorio

---
+++ __openerp__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Colombia - Punto de venta (Terceros)',
+    'category': 'Localization',
+    'version': '0.9',
+    'author': 'Dreamsoft',
+    'license': 'AGPL-3',
+    'maintainer': 'mantenimiento@dreamsoft.com',
+    'website': 'https://www.dreamsoft.com',
+    'summary': 'Colombia Punto de Venta: Extending the Contact Module in the Point of Sales Module - Odoo 9.0',
+    'images': [],
+    'description': """
+Colombia Punto de Venta:
+======================
+_
+    * This Module extends the  l10n_co_point_of_sale / Contact form in the Point of Sales Module
+    """,
+    'depends': [
+        'l10n_co_point_of_sale'
+    ],
+    'data': [],
+    'qweb': [],
+    'installable': True,
+}
