3517996ae6f1dd11faccc5fb9b69e98fdfd3fe47
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2017-03-10 22:52:48 -0500

    agregando cambio, para que de acuerdo a la moneda que tiene la compania por defecto, entonces cuando estemos creando las monedas nos salga por defecto el tipo de moneda de la compania

--- models/tipoMoneda.py
+++ models/tipoMoneda.py
@@ -9,10 +9,17 @@ class moneda(models.Model):
 	
 	_description = 'Se crea la denominacion del tipo de moneda del pais'
 	
+	"""
+	metodo que nos permite de acuerdo a el tipo de moneda que tiene la compania
+	cargar por defecto ese tipo de moneda, cuando estemos creando el valor de estas
+	"""
+	def buscar_moneda_localizacion(self):
+		return self.env['res.company'].browse(self.env.user.company_id.id).currency_id
 	
 	name =  fields.Char('Valor moneda', size=48, required=True)
-	moneda_id = fields.Many2one('res.currency', 'Unidad monetaria')
+	moneda_id = fields.Many2one('res.currency', 'Unidad monetaria', default=buscar_moneda_localizacion)
 	
+
 
 
 moneda()
