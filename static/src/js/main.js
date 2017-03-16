595d074e05281c849c7a724f5867431eae8d3030
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2017-03-15 21:03:51 -0500

    agregado cambio, para tratar de heredar o incluir algo en el archivo

---
+++ static/src/js/main.js
@@ -0,0 +1,12 @@
+odoo.define('pointofsale.main', function(require) {
+"use strict";
+
+var PosDB = require('point_of_sale.DB');
+var core = require('web.core');
+var module = require('point_of_sale.models');
+var Model = require('web.DataModel');
+var gui = require('point_of_sale.gui');
+var screens = require('point_of_sale.screens');
+var _t = core._t;
+
+});
--- static/src/js/screens_inherit.js
+++ static/src/js/screens_inherit.js
@@ -1,51 +1,24 @@
-odoo.define('pointofsale.screens_inherit', function(require) {
-"use strict";
+odoo.define('point_of_sale_screens_inherit.main', function(require) {
+    "use strict";
 
-var PosDB = require('point_of_sale.DB');
-var core = require('web.core');
-var module = require('point_of_sale.models');
-var Model = require('web.DataModel');
-var gui = require('point_of_sale.gui');
-var screens = require('point_of_sale.screens');
-var _t = core._t;
+    var core = require('web.core');
+    var screens = require('point_of_sale.screens');
+    var models = require('point_of_sale.models');
+    var devices = require('point_of_sale.devices');
+    var chrome = require('point_of_sale.chrome');
+    var gui = require('point_of_sale.gui');
+    var popups = require('point_of_sale.popups');
+    var Class = require('web.Class');
+    var utils = require('web.utils');
+    var PosBaseWidget = require('point_of_sale.BaseWidget');
+    var _t = core._t;
 
-var round_pr = utils.round_precision;
-
-
-var set_buscar_carrito_compra = ActionButtonWidget.extend({
-    template: 'BuscarCarritoCompra',
-    button_click: function () {
-        var self = this;
-        var selection_list = _.map(self.pos.fiscal_positions, function (fiscal_position) {
-            return {
-                label: fiscal_position.name,
-                item: fiscal_position
-            };
-        });
-        self.gui.show_popup('selection',{
-            title: _t('Select tax'),
-            list: selection_list,
-            confirm: function (fiscal_position) {
-                var order = self.pos.get_order();
-                order.fiscal_position = fiscal_position;
-                order.trigger('change');
-            }
-        });
-    },
-});
-
-define_action_button({
-    'name': 'set_buscar_carrito',
-    'widget': set_buscar_carrito_compra,
-    'condition': function(){
-        return this.pos.fiscal_positions.length > 0;
-    },
+    var round_pr = utils.round_precision;
 
 
-return {
-    set_buscar_carrito_compra: set_buscar_carrito_compra,
-};
+    screens.include({
 
 
+    });
 
 });
